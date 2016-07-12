dffd44e1b18ac3d1e2e5900688970707343cce75
Daniel Petro <DanielPetro@Daniels-MacBook-Pro.local>
2016-07-12 11:44:49 -0400

    added git files and basic homepage layout structure

---
+++ index.js
@@ -0,0 +1,21 @@
+// Navbar Collapse
+$(".button-collapse").sideNav();
+
+//Parallax
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+
+//Collapsible
+  $(document).ready(function(){
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+  });
+
+//Flickity
+$('.main-carousel').flickity({
+  // options
+  cellAlign: 'left',
+  contain: true
+});
